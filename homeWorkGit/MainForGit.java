1793b3f54a745d1444e40ba73f805835a71d0b56
Andrei Birukou <medialabs91@gmail.com>
2021-01-03 18:47:00 +0300

    Program asking name

--- homeWorkGit/MainForGit.java
+++ homeWorkGit/MainForGit.java
@@ -1,4 +1,13 @@
 package homeWorkGit;
 
+import java.util.Scanner;
+
 public class MainForGit {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input you name");
+        String inputName = scanner.nextLine();
+        System.out.println("Hello " + inputName);
+    }
 }
