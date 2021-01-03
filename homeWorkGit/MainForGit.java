124b4007dedf82d1b2ee84c339f9108b7b4eceea
Andrei Birukou <medialabs91@gmail.com>
2021-01-03 18:57:08 +0300

    Nice to meet you

--- homeWorkGit/MainForGit.java
+++ homeWorkGit/MainForGit.java
@@ -8,6 +8,6 @@ public class MainForGit {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please input you name");
         String inputName = scanner.nextLine();
-        System.out.println("Hello " + inputName);
+        System.out.println("Nice to meet you " + inputName);
     }
 }
